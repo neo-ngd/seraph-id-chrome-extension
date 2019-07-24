24b7e0d7d493ce813b7ac241cd97b194e0092398
Milos Costantini <milos.costantini@swisscom.com>
2019-07-24 16:17:13 +0200

    new structure, redux integration

--- src/utils/storage.js
+++
@@ -1,9 +0,0 @@
-export function checkWallet() {
-  if (localStorage.getItem("wallet") === "null") {
-    return false;
-  } else return true;
-}
-
-export function setWallet(wallet) {
-  localStorage.setItem("wallet", wallet);
-}
