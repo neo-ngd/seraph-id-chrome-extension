273ccbf37a8373333c660cb404fa22fdeba841a8
Milos Costantini <milos.costantini@swisscom.com>
2019-08-02 13:40:07 +0200

    added persistence

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -2,9 +2,15 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import { wrapStore } from 'webext-redux';
 import { composeWithDevTools } from 'remote-redux-devtools';
+import { persistStore, persistReducer } from "redux-persist";
+import storageSession from "redux-persist/lib/storage"; // Use SessionStorage
 
 const composeEnhancers = composeWithDevTools({ realtime: true, hostname: 'localhost', port: 8000 });
-
-const store = createStore(rootReducer, composeEnhancers());
-
+const persistConfig = {
+    key: "root",
+    storage: storageSession
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, composeEnhancers());
 wrapStore(store);
+export const persistor = persistStore(store);
