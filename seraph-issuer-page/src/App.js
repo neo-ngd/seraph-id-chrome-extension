4ff3cb581369715fbe47ed6f18efcc616fcb4787
Milos Costantini <milos.costantini@swisscom.com>
2019-09-26 19:54:06 +0200

    added router

--- seraph-issuer-page/src/App.js
+++ seraph-issuer-page/src/App.js
@@ -1,50 +1,21 @@
 import React from "react";
-import { Grid } from "@material-ui/core/";
-import { makeStyles } from "@material-ui/core/styles";
 import NavBar from "./components/NavBar/NavBar";
 import "./App.css";
-import { createClaim } from "./seraphUtils";
 import IssuerPage from "./scenes/IssuerPage";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1
-  },
-  page: {
-    paddingTop: "90px"
-  }
-}));
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  const classes = useStyles();
-
-  function getSera() {
-    console.log("called");
-
-    const claim = createClaim();
-    window.seraphID.sendClaim(claim);
-  }
-
-  const askClaim = () =>
-    window.seraphID.askClaim("0e5edf34-0451-4eb5-9781-92a413fc6445");
-
   return (
-    <div className={classes.root}>
-      <NavBar></NavBar>
-
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        spacing={3}
-        className={classes.page}
-      >
-        <Grid item xs={12}>
-          <IssuerPage></IssuerPage>
-        </Grid>
-      </Grid>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <div style={{ paddingTop: "90px" }}>
+          <Route exact path="/" component={IssuerPage} />
+          <Route path="/government" component={IssuerPage} />
+          <Route path="/topics" component={IssuerPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
